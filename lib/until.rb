d9cc5ffac2e15c9ddb2d2649edd1e942ef14dbb2
vincent servio <vincentservio@gmail.com>
2019-08-25 18:03:36 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
   levitation_force = 6
+  until levitation_force == 10
+  print "puts out the phrase 'Wingardium Leviosa"
+  levitation_force += 1
+end
   #your code here
     
 end
-
